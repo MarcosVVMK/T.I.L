a702f161a06e1f481fed113af47ecb3759966b5f
Marcos <marcos@indexis.com.br>
2021-06-06 16:55:00 -0300

    finish transferecia screen

--- Flutter/bytebank/lib/main.dart
+++ Flutter/bytebank/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ListaTransferencia(),
         appBar: AppBar(
@@ -14,21 +15,34 @@ class ListaTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('100.0'),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('200.0'),
-            subtitle: Text('200.0'),
-          ),
-        ),
+        ItemTransferencia(Transferencia(100.2, 1000)),
+        ItemTransferencia(Transferencia(230.2, 1001)),
+        ItemTransferencia(Transferencia(3244.2, 1002)),
       ],
     );
   }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numConta;
+
+  Transferencia(this.valor, this.numConta);
 }
