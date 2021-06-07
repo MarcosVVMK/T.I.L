07e912f90fd5bcbba9dc6975cdac0fdffc396b41
Marcos <marcos.matos@academico.ifg.edu.br>
2021-06-07 17:47:16 -0300

    create a new project

---
+++ Flutter/login/lib/pages/login.page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: new TextStyle(color: Colors.white, fontSize: 2),
+                decoration: InputDecoration(
+                  labelText: "CPF",
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
